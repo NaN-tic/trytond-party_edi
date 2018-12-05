acef15bc9957af2227266703ec28a8d44ec20dcb
Carlos G?lvez <carlos.galvez@nan-tic.com>
2018-12-05 14:46:47 +0100

    Adds EAN EDI Code to Address. #036192

--- __init__.py
+++ __init__.py
@@ -2,8 +2,11 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from . import party
+from . import address
+
 
 def register():
     Pool.register(
+        address.Address,
         party.Party,
         module='party_edi', type_='model')
