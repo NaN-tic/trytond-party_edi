c53fc5788d83fa1f26880870badfe84e86972aed
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-02-15 14:04:42 +0100

    Converts the field edi_operatinal_point into a function field and uses the party identifiers to store the EDI operational point. #035224

--- party.py
+++ party.py
@@ -1,9 +1,9 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
 
-__all__ = ['Party']
+__all__ = ['Party', 'PartyIdentifier']
 
 
 class Party:
@@ -11,4 +11,41 @@ class Party:
     __name__ = 'party.party'
 
     allow_edi = fields.Boolean('Allow EDI', help='Allow EDI communications')
-    edi_operational_point = fields.Char('EDI Operational Point', size=35)
+    edi_operational_point = fields.Function(
+        fields.Char('EDI Operational Point', size=35),
+        'get_edi_operational_point', setter='set_edi_operational_point')
+
+    def get_edi_operational_point(self, name=None):
+        for identifier in self.identifiers:
+            if identifier.type == 'edi':
+                return identifier.code
+        return None
+
+    @classmethod
+    def set_edi_operational_point(cls, parties, name, value):
+        Identifier = Pool().get('party.identifier')
+        for party in parties:
+            identifier = Identifier.search([
+                    ('party', '=', party.id),
+                    ('type', '=', 'edi')
+                ], limit=1)
+            if identifier:
+                Identifier.write([identifier[0]], {'code': value})
+            else:
+                Identifier.create([{
+                            'party': party,
+                            'type': 'edi',
+                            'code': value,
+                            }])
+
+
+class PartyIdentifier:
+    __metaclass__ = PoolMeta
+    __name__ = 'party.identifier'
+
+    @classmethod
+    def get_types(cls):
+        types = super(PartyIdentifier, cls).get_types()
+        types.append(('edi', 'EDI Operational Point'))
+        return types
+
