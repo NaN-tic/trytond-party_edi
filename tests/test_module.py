c2a6055ab57e0eeb86edc06f0660e3dd587f9902
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-29 13:08:00 +0200

    Fix tests and view

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,13 +2,27 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from trytond.modules.company.tests import CompanyTestMixin
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
 
 
-class PartyEdiTestCase(CompanyTestMixin, ModuleTestCase):
+class PartyEdiTestCase(ModuleTestCase):
     'Test PartyEdi module'
     module = 'party_edi'
+
+    @with_transaction()
+    def test_party_identifiers(self):
+        'Party Identifiers'
+        pool = Pool()
+        Configuration = pool.get('party.configuration')
+        PartyIdentifier = pool.get('party.identifier')
+
+        _TYPE = ('edi_head', 'EDI Operational Point (Head Office)')
+        self.assertTrue(_TYPE in Configuration.identifier_types.selection)
+        self.assertTrue(_TYPE in PartyIdentifier.get_types())
+        _TYPE = ('edi_pay', 'EDI Operational Point (Who Pays)')
+        self.assertTrue(_TYPE in Configuration.identifier_types.selection)
+        self.assertTrue(_TYPE in PartyIdentifier.get_types())
 
 
 del ModuleTestCase
